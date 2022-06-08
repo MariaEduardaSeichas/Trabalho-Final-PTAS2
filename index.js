08339a3e0ec461388b7c689493c6d2386e0a0df1
Maria Eduarda Ferreira de Oliveira Brito Seichas <85970732+MariaEduardaSeichas@users.noreply.github.com>
2022-06-08 21:14:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+var express = require("express");
+var app = express();
+var {autor, livro} = require("./models");
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended:true
+}))
+
+
+app.get("/autores",  async function(req,res){
+var resultado = await autor.findAll();
+res.json(resultado);
+})
+
+app.get("/autores/:id/livros", async function(req, res){
+ let resultado = await autor.findByPk(req.params.id, {
+  include: "livros"
+ })
+res.json(resultado.livros); 
+})
+
+app.get("/livros/:id/autor", async function(req, res){
+ let resultado = await livro.findByPk(req.params.id, {
+  include: "autor"
+ })
+res.json(resultado.autor); 
+})
+
+app.post("/autores", async function(req,res){
+  var resultado = await autor.create(req.body);
+  res.json(resultado);
+})
+
+app.put("/autores/:id", async function(req, res) {
+  var resultado = await autor.destroy({ where: { id: 
+  req.params.id }});
+  res.json(resultado)
+})
+
+app.delete("/autores/:id", async function(req, res) {
+  var resultado = await autor.destroy({ where: { id: req.params.id }});
+  res.json(resultado)
+})
+
+app.get("/autores/:id", async function(req, res) {
+  var resultado = await autor.findOne(req.params.id);
+  res.send(resultado)
+})
+
+app.get("/livros",  async function(req,res){
+var resultado = await livro.findAll();
+res.json(resultado);
+})
+
+app.post("/livros", async function(req,res){
+  var resultado = await livro.create(req.body);
+  res.json(resultado);
+})
+
+app.put("/livros/:id", async function(req, res) {
+  var resultado = await livro.findByPk(req.params.id);
+  resultado.nome = req.body.nome
+  var salvar = await resultado.save()
+  res.json(salvar)
+})
+
+app.delete("/livros/:id", async function(req, res) {
+  var resultado = await livro.destroy({ where: { id: req.params.id }});
+  res.json(resultado)
+})
+
+app.get("/livros/:id", async function(req, res) {
+  var resultado = await livro.findByPk(req.params.id);
+  res.send(resultado)
+})
+
+app.listen(3001,function(){
+  console.log("O código está funcionando");
+});
---
+++ models/livro.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class livro extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.autor, { as:"autor" })
+    }
+  }
+  livro.init({
+    autorId: DataTypes.INTEGER,
+    titulo: DataTypes.STRING,
+    editora: DataTypes.STRING,
+    data_publicacao: DataTypes.DATE,
+    preco: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'livro',
+  });
+  return livro;
+};
